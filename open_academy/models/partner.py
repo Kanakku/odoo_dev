3ef84e00107d4f373d815089f45a8947c6a8e3b9
Kanakku <bcanaca_24@yahoo.es>
2022-02-22 15:16:42 -0600

    add dominio, herencia

--- open_academy/models/partner.py
+++ open_academy/models/partner.py
@@ -4,5 +4,4 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     instructor = fields.Boolean("Instructor", default=False)
-
     session_ids = fields.Many2many('open_academy.session', string="Attended Sessions", readonly=True)
